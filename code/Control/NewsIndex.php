598a066e784c901c21fa15f6ebf38209dc47b7d9
Nivanka Fonseka <nivanka@silverstripers.com>
2015-04-06 16:14:57 +0530

    filter functions to return news articles

--- code/Control/NewsIndex.php
+++ code/Control/NewsIndex.php
@@ -13,6 +13,7 @@ class NewsIndex extends Page {
 		'ItemsPerPage' 	=> 'Int',
 	);
 
+
 	function getCMSFields(){
 		$fields = parent::getCMSFields();
 
@@ -32,5 +33,81 @@ class NewsIndex extends Page {
 class NewsIndex_Controller extends Page_Controller {
 
 
+	private static $allowed_actions = array(
+		'tag',
+		'archive'
+	);
+
+	public function init(){
+		parent::init();
+	}
+
+	public function tag(){
+		return $this;
+	}
+
+	public function archive(){
+		return $this;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function IsTag(){
+		return $this->request->param('Action') == 'tag';
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function IsArchive(){
+		return $this->request->param('Action') == 'archive';
+	}
+
+
+	/**
+	 * @param int $iOffset
+	 * @return PaginatedList
+	 */
+	public function Items($iOffset = 0){
+		$request = $this->GetRequestForItems($iOffset);
+
+		$items = NewsPost::get()->filter('ParentID', $this->ID);
+
+		if($this->IsTag()){
+			$items = $items->filter('Tag:PartialMatch', $this->request->param('ID'));
+		}
+
+		if($this->IsArchive()){
+			$strPattern = SiteConfig::current_site_config()->ArchivePattern ? : '%Y, %M';
+			$items = $items->where('DATE_FORMAT(\'' . $strPattern .  '\') = \'' . Convert::raw2sql($this->request->param('ID')) . '\'');
+		}
+
+
+		$paginatedList = new PaginatedList($items, $request);
+		$paginatedList->setPageLength($this->ItemsPerPage ? : SiteConfig::current_site_config()->ItemsPerPage ? : 10 );
+		return $paginatedList;
+	}
+
+
+	/**
+	 * @param int $iOffset
+	 * @return NullHTTPRequest|SS_HTTPRequest
+	 */
+	function GetRequestForItems($iOffset = 0){
+		if($iOffset == 0)
+			return $this->request;
+
+		$iStart = 0;
+		$request = Controller::curr()->request;
+		if($request->getVar('start')){
+			$iStart = $request->getVar('start');
+		}
+		$iStart += $iOffset;
+
+		return new SS_HTTPRequest("get", "/", array(
+			"start"		=> $iStart,
+		));
+	}
 
 }
