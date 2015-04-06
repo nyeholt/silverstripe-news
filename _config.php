8cd9606469a210842fa6316aa6c23ccf3800cff7
Nivanka Fonseka <nivanka@silverstripers.com>
2015-04-06 15:25:22 +0530

    update the hierarchy and only get the news pages to be edited on the News model admin

--- _config.php
+++ _config.php
@@ -5,4 +5,8 @@
  * Date: 4/6/15
  * Time: 2:03 PM
  * To change this template use File | Settings | File Templates.
- */ 
+ */
+
+
+SiteTree::remove_extension('Hierarchy');
+Object::add_extension('SiteTree', 'NewsHierarchy');
---
+++ code/Extensions/NewsHierarchy.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by Nivanka Fonseka (nivanka@silverstripers.com).
+ * User: nivankafonseka
+ * Date: 4/6/15
+ * Time: 3:07 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class NewsHierarchy extends Hierarchy {
+
+	public function getChildrenAsUL($attributes = "", $titleEval = '"<li>" . $child->Title', $extraArg = null,
+									$limitToMarked = false, $childrenMethod = "AllChildrenIncludingDeleted",
+									$numChildrenMethod = "numChildren", $rootCall = true,
+									$nodeCountThreshold = null, $nodeCountCallback = null) {
+
+		if($this->owner->ClassName == 'NewsIndex'){
+			$strURL = Director::baseURL() . '/admin/news?ParentID=' . $this->owner->ID;
+			$output = "<ul$attributes>\n";
+			$output.= '<li class="readonly">
+				<a class="cms-panel-link" data-pjax-target="Content" href="' . $strURL . '">Use News admin to manage pages of this tree</a>
+			</li>';
+			$output.= "</ul>";
+			return $output;
+		}
+		else{
+			return parent::getChildrenAsUL($attributes, $titleEval, $extraArg, $limitToMarked, $childrenMethod, $numChildrenMethod,
+				$rootCall, $nodeCountThreshold, $nodeCountCallback);
+		}
+	}
+
+} 
