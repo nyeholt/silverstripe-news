2d01e923aa4fea7dea08ade329784692baafb46a
Nivanka Fonseka <nivanka@silverstripers.com>
2015-10-29 11:37:34 +0530

    in hierarchy remove the news posts and show a link but give the flexibility to add other pages

--- code/Extensions/NewsHierarchy.php
+++ code/Extensions/NewsHierarchy.php
@@ -17,7 +17,13 @@ class NewsHierarchy extends Hierarchy {
 
 		if(get_class($this->owner) == 'NewsIndex'){
 			$strURL = $this->owner->getNewsItemsEditLink();
-			$output = "<ul$attributes>\n";
+
+
+			$original =  parent::getChildrenAsUL($attributes, $titleEval, $extraArg, $limitToMarked, $childrenMethod, $numChildrenMethod,
+				$rootCall, $nodeCountThreshold, $nodeCountCallback);
+
+			$output = $original ? $original : "";
+			$output.= "<ul$attributes>\n";
 			$output.= '<li class="readonly">
 				<a class="cms-panel-link" data-pjax-target="Content" href="' . $strURL . '">' . $this->owner->getTreeEditLinkText() .  '</a>
 			</li>';
@@ -43,6 +49,47 @@ class NewsHierarchy extends Hierarchy {
 				$ext->clearOwner();
 			}
 		}
+	}
+
+	public function doAllChildrenIncludingDeleted($context = null) {
+		if(!$this->owner) user_error('Hierarchy::doAllChildrenIncludingDeleted() called without $this->owner');
+
+		$baseClass = ClassInfo::baseDataClass($this->owner->class);
+		if($baseClass) {
+			$stageChildren = $this->owner->stageChildren(true);
+			$stageChildren = $this->RemoveNewsPostsFromSiteTree($stageChildren);
+
+			// Add live site content that doesn't exist on the stage site, if required.
+			if($this->owner->hasExtension('Versioned')) {
+				// Next, go through the live children.  Only some of these will be listed
+				$liveChildren = $this->owner->liveChildren(true, true);
+				if($liveChildren) {
+					$liveChildren = $this->RemoveNewsPostsFromSiteTree($liveChildren);
+					$merged = new ArrayList();
+					$merged->merge($stageChildren);
+					$merged->merge($liveChildren);
+					$stageChildren = $merged;
+				}
+			}
+
+			$this->owner->extend("augmentAllChildrenIncludingDeleted", $stageChildren, $context);
+
+		} else {
+			user_error("Hierarchy::AllChildren() Couldn't determine base class for '{$this->owner->class}'",
+				E_USER_ERROR);
+		}
+
+		return $stageChildren;
+	}
+
+
+	public function RemoveNewsPostsFromSiteTree(DataList $list){
+		$pageTypes = NewsPost::GetNewsTypes();
+		$updatedList = $list;
+		foreach($pageTypes as $strClassName){
+			$updatedList = $updatedList->exclude('ClassName', $strClassName);
+		}
+		return $updatedList;
 	}
 
 } 
