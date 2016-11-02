e5f23ebdc4dddf056f64db9e88ae3bf1b7bcebc0
Marcus Nyeholt <marcus@silverstripe.com.au>
2016-11-02 22:13:33 +1100

    FIX Re-introduced missing RSS feed output

--- code/pages/NewsHolder.php
+++ code/pages/NewsHolder.php
@@ -291,5 +291,18 @@ class NewsHolder extends Page {
 }
 
 class NewsHolder_Controller extends Page_Controller {
-	
+	public static $allowed_actions = array('Rss');
+
+    public function init() {
+        RSSFeed::linkToFeed($this->owner->Link() . "rss", _t('News.RSSLINK',"RSS feed for the News"));
+        parent::init();
+    }
+
+    function Rss() {
+        $parent = $this->data()->ID;
+        $objects = NewsArticle::get()->filter('ParentID', $parent)->sort('LastEdited DESC')->limit(10);
+        $rss = new RSSFeed($objects, $this->data()->Link(), _t('News.RSSTITLE',"10 most recent news"), "", "Title", "Content");
+        $this->response->addHeader('Content-Type', 'application/rss+xml');
+        return $rss->outputToBrowser();
+    }
 }
